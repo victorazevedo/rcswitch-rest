ce3d1e3e1d6f967b0601c3078dffa124024ea777
victorazevedo <v.azevedo9@gmail.com>
2016-04-27 04:12:37 +0000

    add 3 portas gpio

--- gpio_test.js
+++ gpio_test.js
@@ -1,4 +1,18 @@
 var GPIO = require('onoff').Gpio,
+led20 = new GPIO(20, 'out'),
 led21 = new GPIO(21, 'out'),
+led26 = new GPIO(26, 'out');
+
+led20.writeSync(1);
 led21.writeSync(1);
- 
+led26.writeSync(1);
+
+setTimeout(function(){
+
+led20.writeSync(0);
+led21.writeSync(0);
+led26.writeSync(0);
+
+
+},2000);
+
